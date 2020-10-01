e6b2e0293401afdc620231f8d113e929cfef9914
Indraneel Sarkar <indraneel.sarkar@ET-C02PR0FSG8WN.local>
2020-10-01 15:12:11 +0530

    https://leetcode.com/problems/container-with-most-water/

---
+++ Container.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int ans=0, left=0, right=height.length-1;
+        while(left<right){
+            ans=Math.max(ans,Math.min(height[left],height[right])*(right-left));
+            if(height[left]<height[right])
+                left++;
+            else
+                right--;
+        }
+        return ans;
+    }
+}
